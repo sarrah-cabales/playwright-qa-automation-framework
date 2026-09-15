69359fdef7dc7c51414da920e189294140eda28c
Sarrah <sarrah@Sarrahs-MacBook-Air.local>
2026-09-15 12:31:15 +0800

    config(visual):update snapshot path template to ignore platform suffix

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,13 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+
+  /* Configure custom snapshot path template to share baseline across OS platforms */
+  expect: {
+    toHaveScreenshot: {
+      pathTemplate: '{testDir}/{testFileDir}/__snapshots__/{testFileName}-snapshots/{arg}{ext}',
+    },
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
